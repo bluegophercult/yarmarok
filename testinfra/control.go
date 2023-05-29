1e8a1a2e09e7136b394532dacdffe1cbb05a7424
radikh <13639129+radikh@users.noreply.github.com>
2023-05-29 21:17:19 +0300

    testinfra: add fucntion comment.

--- testinfra/control.go
+++ testinfra/control.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// SkipIfNotIntegrationRun skips the test if RUN_INTEGRATION_TESTS is not set to true.
 func SkipIfNotIntegrationRun(t *testing.T) {
 	if os.Getenv("RUN_INTEGRATION_TESTS") != "true" {
 		t.Skip("Skipping integration test")
