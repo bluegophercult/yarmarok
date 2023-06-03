8ea2696567b8507a7e96909ee687074497dd03e3
delveper <yevhen@bilyk.dev>
2023-06-03 10:57:42 +0300

    Add participant storage impl

---
+++ service/participant.go
@@ -0,0 +1,28 @@
+package service
+
+import "errors"
+
+var (
+	ErrParticipantAlreadyExists = errors.New("participant already exists")
+	ErrParticipantNotFound      = errors.New("participant not found")
+)
+
+// Participant represents a participant of the application.
+type Participant struct {
+	ID         string
+	YarmarokID string
+	Name       string
+	Phone      string
+	Email      string
+	Notes      string
+}
+
+// ParticipantStorage is a storage for participants.
+type ParticipantStorage interface {
+	Create(Participant) error
+	Get(id string) (*Participant, error)
+	Update(Participant) error
+	Delete(id string) error
+	GetAll() ([]Participant, error)
+	Exists(id string) (bool, error)
+}
---
+++ storage/participant.go
@@ -0,0 +1,134 @@
+package storage
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"fmt"
+	"github.com/kaznasho/yarmarok/service"
+)
+
+const ()
+
+// FirestoreParticipantStorage is a storage for yarmaroks based on Firestore.
+type FirestoreParticipantStorage struct {
+	userID          string
+	firestoreClient *firestore.CollectionRef
+}
+
+// ParticipantStorage returns a participant storage.
+func (us *FirestoreUserStorage) ParticipantStorage(userID string) *FirestoreParticipantStorage {
+	return NewFirestoreParticipantStorage(us.firestoreClient.Doc(userID).Collection(participantCollection), userID)
+}
+
+// NewFirestoreParticipantStorage creates a new FirestoreParticipantStorage.
+func NewFirestoreParticipantStorage(client *firestore.CollectionRef, userID string) *FirestoreParticipantStorage {
+	return &FirestoreParticipantStorage{
+		userID:          userID,
+		firestoreClient: client,
+	}
+}
+
+// Create creates a new participant.
+func (ps *FirestoreParticipantStorage) Create(p service.Participant) error {
+	exists, err := ps.Exists(p.ID)
+	if err != nil {
+		return fmt.Errorf("check participant exists: %w", err)
+	}
+
+	if exists {
+		return service.ErrParticipantAlreadyExists
+	}
+
+	if _, err := ps.firestoreClient.Doc(p.ID).Set(context.Background(), p); err != nil {
+		return fmt.Errorf("create participant: %w", err)
+	}
+
+	return nil
+}
+
+// Get returns a participant with the given ID.
+func (ps *FirestoreParticipantStorage) Get(id string) (*service.Participant, error) {
+	doc, err := ps.firestoreClient.Doc(id).Get(context.Background())
+	if err != nil {
+		if isNotFound(err) {
+			return nil, service.ErrParticipantNotFound
+		}
+		return nil, fmt.Errorf("get participant: %w", err)
+	}
+
+	var p service.Participant
+	if err = doc.DataTo(&p); err != nil {
+		return nil, fmt.Errorf("decode participant: %w", err)
+	}
+
+	return &p, nil
+}
+
+// Update updates an existing participant.
+func (ps *FirestoreParticipantStorage) Update(p service.Participant) error {
+	exists, err := ps.Exists(p.ID)
+	if err != nil {
+		return fmt.Errorf("check participant exists: %w", err)
+	}
+
+	if !exists {
+		return service.ErrParticipantNotFound
+	}
+
+	if _, err := ps.firestoreClient.Doc(p.ID).Set(context.Background(), p); err != nil {
+		return fmt.Errorf("update participant: %w", err)
+	}
+
+	return nil
+}
+
+// Delete deletes a participant.
+func (ps *FirestoreParticipantStorage) Delete(id string) error {
+	exists, err := ps.Exists(id)
+	if err != nil {
+		return fmt.Errorf("check participant exists: %w", err)
+	}
+
+	if !exists {
+		return service.ErrParticipantNotFound
+	}
+
+	if _, err := ps.firestoreClient.Doc(id).Delete(context.Background()); err != nil {
+		return fmt.Errorf("delete participant: %w", err)
+	}
+
+	return nil
+}
+
+// GetAll returns a list of all participants.
+func (ps *FirestoreParticipantStorage) GetAll() ([]service.Participant, error) {
+	docs, err := ps.firestoreClient.Documents(context.Background()).GetAll()
+	if err != nil {
+		return nil, fmt.Errorf("get participant: %w", err)
+	}
+
+	participants := make([]service.Participant, 0, len(docs))
+	for _, doc := range docs {
+		var p service.Participant
+		if err = doc.DataTo(&p); err != nil {
+			return nil, fmt.Errorf("decode participants: %w", err)
+		}
+
+		participants = append(participants, p)
+	}
+
+	return participants, nil
+}
+
+func (ps *FirestoreParticipantStorage) Exists(id string) (bool, error) {
+	doc, err := ps.firestoreClient.Doc(id).Get(context.Background())
+	if isNotFound(err) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	return doc.Exists(), nil
+}
