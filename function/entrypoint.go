fbf84ff128e50ab328f44f61c9d69e71dd37988c
Artem Yadelskyi <mymmrac@gmail.com>
2023-07-10 12:26:34 +0300

    Added local test run

--- function/entrypoint.go
+++ function/entrypoint.go
@@ -8,18 +8,19 @@ import (
 	"os"
 
 	"cloud.google.com/go/firestore"
+
 	"github.com/kaznasho/yarmarok/logger"
 	"github.com/kaznasho/yarmarok/service"
 	"github.com/kaznasho/yarmarok/storage"
 	"github.com/kaznasho/yarmarok/web"
 )
 
-const projectIDEnvVar = "GCP_PROJECT"
+const ProjectIDEnvVar = "GCP_PROJECT"
 
 // ErrEmptyProjectID is returned when the project id is empty.
 var ErrEmptyProjectID = errors.New("empty project id")
 
-// EntryPoint is the entry point for the cloud function.
+// Entrypoint is the entry point for the cloud function.
 func Entrypoint(w http.ResponseWriter, r *http.Request) {
 	log := logger.NewLogger(logger.LevelInfo)
 
@@ -35,9 +36,9 @@ func Entrypoint(w http.ResponseWriter, r *http.Request) {
 
 // LoadRouter loads the router.
 func LoadRouter(log *logger.Logger) (*web.Router, error) {
-	projectID := os.Getenv(projectIDEnvVar)
+	projectID := os.Getenv(ProjectIDEnvVar)
 	if projectID == "" {
-		return nil, fmt.Errorf("%w: %s is not set", ErrEmptyProjectID, projectIDEnvVar)
+		return nil, fmt.Errorf("%w: %s is not set", ErrEmptyProjectID, ProjectIDEnvVar)
 	}
 
 	firestoreClient, err := firestore.NewClient(context.Background(), projectID)
