3b66273f90c6f0c18b83a5f9ec3c0f5984331986
delveper <yevhen@bilyk.dev>
2023-07-31 08:58:37 +0300

    feat: middlewares

--- web/middlewares.go
+++ web/middlewares.go
@@ -1,13 +1,140 @@
 package web
 
-// Middleware is used to process HTTP requests before they reach the final Handler.
+import (
+	"errors"
+	"net/http"
+	"runtime/debug"
+	"time"
+
+	"github.com/kaznasho/yarmarok/service"
+	"github.com/rs/cors"
+
+	"github.com/kaznasho/yarmarok/logger"
+)
+
+var (
+	ErrCreatingOrganizer  = errors.New("creating organizer")
+	ErrRecoveredFromPanic = errors.New("recovered from panic")
+)
+
 type Middleware = func(Handler) Handler
 
-// WrapMiddlewares chains a series of Middleware applied from top to bottom order.
 func WrapMiddlewares(h Handler, mws ...Middleware) Handler {
 	for i := len(mws) - 1; i >= 0; i-- {
 		h = mws[i](h)
 	}
 
 	return h
+}
+
+// WithErrors is a middleware that handles errors if they occur during the execution.
+// If the error is not of the type Error, as determined by the ErrorAs function, then a default error is created.
+// Log and Message data will be logged or sent to a client respectively.
+func WithErrors(log *logger.Logger) Middleware {
+	return func(h Handler) Handler {
+		return func(rw http.ResponseWriter, req *http.Request) error {
+			if err := h(rw, req); err != nil {
+				errweb, ok := ErrorAs(err)
+				if !ok {
+					errweb = NewError(err, http.StatusInternalServerError, Message(logger.Fields{"error": ErrUnknownError}))
+				}
+
+				log.WithError(err).WithFields(logger.Fields(errweb.Log))
+
+				return Respond(rw, errweb)
+			}
+
+			return nil
+		}
+	}
+}
+
+func WithJSON(h Handler) Handler {
+	return func(rw http.ResponseWriter, req *http.Request) error {
+		rw.Header().Set("Content-Type", "application/json")
+
+		return h(rw, req)
+	}
+}
+
+func WithXLSX(h Handler) Handler {
+	return func(rw http.ResponseWriter, req *http.Request) error {
+		rw.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+		return h(rw, req)
+	}
+}
+
+func WithOrganizer(svc service.OrganizerService, log *logger.Logger) Middleware {
+	return func(h Handler) Handler {
+		return func(rw http.ResponseWriter, req *http.Request) error {
+			organizerID, err := extractOrganizerID(req)
+			if err != nil {
+				return NewError(errors.Join(ErrMissingID, err), http.StatusBadRequest)
+			}
+
+			if err = svc.CreateOrganizerIfNotExists(organizerID); err != nil {
+				return NewError(errors.Join(ErrCreatingOrganizer, err), http.StatusInternalServerError)
+			}
+
+			return h(rw, req)
+		}
+	}
+}
+
+func WithLogging(log *logger.Logger) Middleware {
+	return func(h Handler) Handler {
+		return func(rw http.ResponseWriter, req *http.Request) error {
+			start := time.Now()
+
+			defer func() {
+				log.WithFields(logger.Fields{
+					"uri":          req.RequestURI,
+					"method":       req.Method,
+					"status":       rw.Header().Get("Status"),
+					"duration":     time.Since(start),
+					"size":         rw.Header().Get("Content-Length"),
+					"organizer_id": req.Header.Get(GoogleUserIDHeader),
+				}).Info("request completed")
+			}()
+
+			return h(rw, req)
+		}
+	}
+}
+
+func WithRecover(log *logger.Logger) Middleware {
+	return func(h Handler) Handler {
+		return func(rw http.ResponseWriter, req *http.Request) (err error) {
+			defer func() {
+				if rec := recover(); rec != nil {
+					log.WithFields(logger.Fields{
+						"uri":    req.RequestURI,
+						"method": req.Method,
+						"rec":    rec,
+						"trace":  string(debug.Stack()),
+					}).Error(ErrRecoveredFromPanic)
+
+					err = NewError(ErrRecoveredFromPanic, http.StatusInternalServerError, Fields{"error": ErrUnknownError})
+				}
+			}()
+
+			return h(rw, req)
+		}
+	}
+}
+
+func WithCORS(h Handler) Handler {
+	return func(rw http.ResponseWriter, req *http.Request) error {
+		c := cors.New(cors.Options{
+			AllowedOrigins:   []string{defaultOrigin},
+			AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Goog-Authenticated-User-Id"},
+			ExposedHeaders:   []string{},
+			AllowCredentials: true,
+			MaxAge:           0,
+		})
+		c.HandlerFunc(rw, req)
+
+		return h(rw, req)
+	}
 }
---
+++ web/middlewares_test.go
@@ -0,0 +1,243 @@
+package web
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/kaznasho/yarmarok/logger"
+	"github.com/kaznasho/yarmarok/web/mocks"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithErrors(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	osMock := mocks.NewMockOrganizerService(ctrl)
+	organizerID := "organizer_id_1"
+
+	log := logger.NewNoOpLogger()
+
+	web := NewWeb(logger.NewNoOpLogger(), osMock)
+	require.NotNil(t, web)
+
+	t.Run("default_error", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		mockedErr := errors.New("mocked error")
+		h := newStubHandler(mockedErr)
+		rw := httptest.NewRecorder()
+
+		err = WithErrors(log)(h.ServeHTTP)(rw, req)
+		require.Nil(t, err)
+
+		require.Equal(t, http.StatusInternalServerError, rw.Code)
+		require.True(t, h.Called())
+	})
+
+	t.Run("custom_error", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		mockedErr := errors.New("mocked error")
+		errweb := NewError(mockedErr, http.StatusBadRequest)
+		h := newStubHandler(errweb)
+		rw := httptest.NewRecorder()
+
+		require.Nil(t, WithErrors(log)(h.ServeHTTP)(rw, req))
+		require.Equal(t, http.StatusBadRequest, rw.Code)
+		require.True(t, h.Called())
+	})
+
+	t.Run("no_error", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		h := newStubHandler()
+		rw := httptest.NewRecorder()
+
+		require.Nil(t, WithErrors(log)(h.ServeHTTP)(rw, req))
+		require.Equal(t, http.StatusOK, rw.Code)
+		require.True(t, h.Called())
+	})
+}
+
+func TestWithOrganizer(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	osMock := mocks.NewMockOrganizerService(ctrl)
+	organizerID := "organizer_id_1"
+
+	web := NewWeb(logger.NewNoOpLogger(), osMock)
+	require.NotNil(t, web)
+
+	log := logger.NewNoOpLogger()
+
+	t.Run("success", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+		osMock.EXPECT().CreateOrganizerIfNotExists(organizerID).Return(nil)
+
+		h := newStubHandler()
+		rw := httptest.NewRecorder()
+
+		require.NoError(t, WithOrganizer(osMock, log)(h.ServeHTTP)(rw, req))
+		require.Equal(t, http.StatusOK, rw.Code)
+		require.True(t, h.Called())
+	})
+
+	t.Run("no_organizer_id", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+
+		h := newStubHandler()
+		rw := httptest.NewRecorder()
+
+		err = WithOrganizer(osMock, log)(h.ServeHTTP)(rw, req)
+		errweb, ok := ErrorAs(err)
+		require.True(t, ok)
+		require.Equal(t, http.StatusBadRequest, errweb.StatusCode())
+		require.False(t, h.Called())
+	})
+
+	t.Run("error", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		mockedErr := errors.New("mocked error")
+		osMock.EXPECT().CreateOrganizerIfNotExists(organizerID).Return(mockedErr)
+
+		h := newStubHandler()
+		rw := httptest.NewRecorder()
+
+		err = WithOrganizer(osMock, log)(h.ServeHTTP)(rw, req)
+		e, ok := ErrorAs(err)
+		require.True(t, ok)
+		require.Error(t, e)
+		require.Equal(t, http.StatusInternalServerError, e.StatusCode())
+
+		require.False(t, h.Called())
+	})
+}
+
+func TestWithCORS(t *testing.T) {
+	web := NewWeb(logger.NewNoOpLogger(), nil)
+	require.NotNil(t, web)
+
+	t.Run("success", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+
+		h := newStubHandler()
+		rw := httptest.NewRecorder()
+
+		require.NoError(t, WithCORS(h.ServeHTTP)(rw, req))
+		require.Equal(t, http.StatusOK, rw.Code)
+		require.Equal(t, defaultOrigin, rw.Header().Get("Access-Control-Allow-Origin"))
+		require.True(t, h.Called())
+	})
+
+	t.Run("no_origin", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, RafflesPath, emptyBody())
+		require.NoError(t, err)
+
+		h := func(rw http.ResponseWriter, r *http.Request) error { return nil }
+		rw := httptest.NewRecorder()
+
+		err = WithCORS(h)(rw, req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rw.Code)
+		require.Equal(t, "", rw.Header().Get("Access-Control-Allow-Origin"))
+	})
+
+	t.Run("wrong_origin", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, RafflesPath, emptyBody())
+		require.NoError(t, err)
+		req.Header.Set("Origin", "wrong_origin")
+
+		h := func(rw http.ResponseWriter, r *http.Request) error { return nil }
+		rw := httptest.NewRecorder()
+
+		err = WithCORS(h)(rw, req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rw.Code)
+		require.Equal(t, "", rw.Header().Get("Access-Control-Allow-Origin"))
+	})
+}
+
+func TestWithRecover(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	osMock := mocks.NewMockOrganizerService(ctrl)
+	organizerID := "organizer_id_1"
+
+	log := logger.NewNoOpLogger()
+
+	web := NewWeb(logger.NewNoOpLogger(), osMock)
+	require.NotNil(t, web)
+
+	t.Run("panic recovery", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		h := func(rw http.ResponseWriter, r *http.Request) error { panic("test panic") }
+		rw := httptest.NewRecorder()
+
+		err = WithRecover(log)(h)(rw, req)
+		errweb, ok := ErrorAs(err)
+		require.True(t, ok)
+		require.ErrorIs(t, errweb.Value, ErrRecoveredFromPanic)
+		require.Equal(t, http.StatusInternalServerError, errweb.StatusCode())
+	})
+
+	t.Run("no panic", func(t *testing.T) {
+		req, err := newRequestWithOrigin(http.MethodPost, RafflesPath, nil)
+		require.NoError(t, err)
+		req.Header.Set(GoogleUserIDHeader, organizerID)
+
+		h := func(rw http.ResponseWriter, r *http.Request) error { return nil }
+		rw := httptest.NewRecorder()
+
+		err = WithRecover(log)(h)(rw, req)
+		require.Nil(t, err)
+		require.Equal(t, http.StatusOK, rw.Code)
+	})
+}
+
+type handlerStub struct {
+	err    error
+	called bool
+	once   sync.Once
+}
+
+func newStubHandler(errs ...error) *handlerStub {
+	return &handlerStub{
+		once:   sync.Once{},
+		called: false,
+		err:    errors.Join(errs...),
+	}
+}
+
+func (h *handlerStub) ServeHTTP(_ http.ResponseWriter, _ *http.Request) error {
+	h.once.Do(func() {
+		h.called = true
+	})
+
+	return h.err
+}
+
+func (h *handlerStub) Called() bool {
+	return h.called
+}
