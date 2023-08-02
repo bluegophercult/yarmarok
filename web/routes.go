c88297ce6b3f727a40d06b13f12f9e40c9ef8a74
delveper <yevhen@bilyk.dev>
2023-08-02 09:48:15 +0300

    feat: prize endpoint

--- web/routes.go
+++ web/routes.go
@@ -13,12 +13,15 @@ const (
 	ApiPath                  = "/api"
 	RafflesPath              = "/raffles"
 	ParticipantsPath         = "/participants"
+	PrizePath                = "/prizes"
 	raffleIDParam            = "raffle_id"
 	participantIDParam       = "participant_id"
 	raffleIDPlaceholder      = "/{" + raffleIDParam + "}"
 	participantIDPlaceholder = "/{" + participantIDParam + "}"
+	prizeIDPlaceholder       = "/{" + raffleIDParam + "}"
 	rafflesGroup             = ApiPath + RafflesPath
 	participantsGroup        = rafflesGroup + raffleIDPlaceholder + ParticipantsPath
+	prizeGroup               = rafflesGroup + raffleIDPlaceholder + PrizePath
 )
 
 // localRun is true if app is build for local run
@@ -42,12 +45,19 @@ func (w *Web) Routes() {
 	w.Handle(http.MethodGet, participantsGroup, "/", w.listParticipants)
 	w.Handle(http.MethodPut, participantsGroup, participantIDPlaceholder, w.updateParticipant)
 	w.Handle(http.MethodDelete, participantsGroup, participantIDPlaceholder, w.deleteParticipant)
+
+	w.Handle(http.MethodPost, prizeGroup, "/", w.createPrize)
+	w.Handle(http.MethodGet, prizeGroup, "/", w.listPrizes)
+	w.Handle(http.MethodPut, prizeGroup, prizeIDPlaceholder, w.updatePrize)
+	w.Handle(http.MethodDelete, prizeGroup, prizeIDPlaceholder, w.deletePrize)
 }
 
 func (w *Web) loginHandler(rw http.ResponseWriter, req *http.Request) error {
 	http.RedirectHandler("/", http.StatusSeeOther).ServeHTTP(rw, req)
 	return nil
 }
+
+/*Raffle */
 
 func (w *Web) createRaffle(rw http.ResponseWriter, req *http.Request) error {
 	svc, err := w.getRaffleService(req)
@@ -92,6 +102,8 @@ func (w *Web) downloadRaffleXLSX(rw http.ResponseWriter, req *http.Request) erro
 	return nil
 }
 
+/* Participant */
+
 func (w *Web) createParticipant(rw http.ResponseWriter, req *http.Request) error {
 	svc, err := w.getParticipantService(req)
 	if err != nil {
@@ -128,7 +140,54 @@ func (w *Web) listParticipants(rw http.ResponseWriter, req *http.Request) error 
 	return newList(svc.List).Handle(rw, req)
 }
 
-/* TOOLING */
+/* Prize */
+
+func (w *Web) createPrize(rw http.ResponseWriter, req *http.Request) error {
+	svc, err := w.getPrizeService(req)
+	if err != nil {
+		return err
+	}
+	return newCreate(svc.Create).Handle(rw, req)
+}
+
+func (w *Web) updatePrize(rw http.ResponseWriter, req *http.Request) error {
+	svc, err := w.getPrizeService(req)
+	if err != nil {
+		return err
+	}
+	return newUpdate(svc.Edit).Handle(rw, req)
+}
+
+func (w *Web) deletePrize(rw http.ResponseWriter, req *http.Request) error {
+	svc, err := w.getPrizeService(req)
+	if err != nil {
+		return err
+	}
+	return newDelete(svc.Delete).Handle(rw, req)
+}
+func (w *Web) listPrizes(rw http.ResponseWriter, req *http.Request) error {
+	svc, err := w.getPrizeService(req)
+	if err != nil {
+		return err
+	}
+	return newList(svc.List).Handle(rw, req)
+}
+
+/* Tooling */
+
+func (w *Web) getPrizeService(req *http.Request) (service.PrizeService, error) {
+	raffleService, err := w.getRaffleService(req)
+	if err != nil {
+		return nil, err
+	}
+
+	raffleID, err := extractRaffleID(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return raffleService.PrizeService(raffleID), nil
+}
 
 func (w *Web) getParticipantService(req *http.Request) (service.ParticipantService, error) {
 	raffleService, err := w.getRaffleService(req)
@@ -136,9 +195,9 @@ func (w *Web) getParticipantService(req *http.Request) (service.ParticipantServi
 		return nil, err
 	}
 
-	raffleID, err := extractParam(req, raffleIDParam)
-	if err != nil || raffleID == "" {
-		return nil, NewError(ErrMissingID, http.StatusBadRequest)
+	raffleID, err := extractRaffleID(req)
+	if err != nil {
+		return nil, err
 	}
 
 	return raffleService.ParticipantService(raffleID), nil
@@ -151,6 +210,14 @@ func (w *Web) getRaffleService(req *http.Request) (service.RaffleService, error)
 	}
 
 	return w.svc.RaffleService(organizerID), nil
+}
+
+func extractRaffleID(req *http.Request) (id string, err error) {
+	raffleID, err := extractParam(req, raffleIDParam)
+	if err != nil || raffleID == "" {
+		return "", NewError(ErrMissingID, http.StatusBadRequest)
+	}
+	return raffleID, nil
 }
 
 func extractOrganizerID(r *http.Request) (id string, err error) {
