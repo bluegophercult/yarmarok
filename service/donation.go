8763f91e07e09ed2d467ff374387d6b3a32f52ab
Anton-Hudz <98592774+Anton-Hudz@users.noreply.github.com>
2023-08-07 23:50:53 +0300

    feat: added donation part(CRUD; service and storage) (#44)
    * feat: added donation part(CRUD; service and storage)
    
    * Remove vendor from Git history
    
    * fix: changed naming in prize and prize_test
    
    * feat/fix: added err wrapping + added tests to donation part
    
    * service: fix init result type.
    
    * service: add json tags to donations.
    
    ---------
    
    Co-authored-by: radikh <13639129+radikh@users.noreply.github.com>

---
+++ service/donation.go
@@ -0,0 +1,101 @@
+package service
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrDonationAlreadyExists = errors.New("donation already exists")
+	ErrDonationNotFound      = errors.New("donation not found")
+)
+
+type Donation struct {
+	ID            string    `json:"id"`
+	PrizeID       string    `json:"prizeID"`
+	ParticipantID string    `json:"participantID"`
+	Amount        int       `json:"amount"`
+	TicketNumber  int       `json:"ticketNumber"`
+	Description   string    `json:"description"`
+	CreatedAt     time.Time `json:"createdAt"`
+}
+
+type DonationAddRequest struct {
+	Amount      int    `json:"amount"`
+	Description string `json:"description"`
+}
+
+type DonationEditRequest Donation
+
+type DonationListResult struct {
+	Donations []Donation `json:"donations"`
+}
+
+type DonationService interface {
+	AddDonation(d *DonationAddRequest) (*CreateResult, error)
+	EditDonation(d *DonationEditRequest) (*Result, error)
+	ListDonation() (*DonationListResult, error)
+	// DeleteDonation????
+}
+
+type DonationStorage interface {
+	Create(participantStorage ParticipantStorage, prizeStorage PrizeStorage, d *Donation) error
+	Get(id string) (*Donation, error)
+	Update(*Donation) error
+	GetAll() ([]Donation, error)
+	Delete(id string) error
+}
+
+type DonationManager struct {
+	donationStorage    DonationStorage
+	participantStorage ParticipantStorage
+	prizeStorage       PrizeStorage
+}
+
+func NewDonationManager(ds DonationStorage, ps ParticipantStorage, pzs PrizeStorage) *DonationManager {
+	return &DonationManager{
+		donationStorage:    ds,
+		participantStorage: ps,
+		prizeStorage:       pzs,
+	}
+}
+
+func (dm *DonationManager) AddDonation(d *DonationAddRequest) (*CreateResult, error) {
+	donation := toDonation(d)
+	if err := dm.donationStorage.Create(dm.participantStorage, dm.prizeStorage, donation); err != nil {
+		return nil, err
+	}
+
+	return &CreateResult{ID: donation.ID}, nil
+}
+
+func (dm *DonationManager) EditDonation(d *DonationEditRequest) (*Result, error) {
+	donation, err := dm.donationStorage.Get(d.ID)
+	if err != nil {
+		return &Result{StatusError}, err
+	}
+
+	if err := dm.donationStorage.Update(donation); err != nil {
+		return &Result{StatusError}, err
+	}
+
+	return &Result{StatusSuccess}, nil
+}
+
+func (dm *DonationManager) ListDonation() (*DonationListResult, error) {
+	donations, err := dm.donationStorage.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return &DonationListResult{Donations: donations}, nil
+}
+
+func toDonation(d *DonationAddRequest) *Donation {
+	return &Donation{
+		ID:          stringUUID(),
+		Amount:      d.Amount,
+		Description: d.Description,
+		CreatedAt:   timeNow(),
+	}
+}
---
+++ storage/donation.go
@@ -0,0 +1,140 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+
+	"github.com/kaznasho/yarmarok/service"
+)
+
+type FirestoreDonationStorage struct {
+	participantID   string
+	firestoreClient *firestore.CollectionRef
+}
+
+func (rs *FirestoreRaffleStorage) DonationStorage(participantID string) service.DonationStorage {
+	return NewFirestoreDonationStorage(rs.collectionReference.Doc(participantID).Collection(participantCollection), participantID)
+}
+
+func NewFirestoreDonationStorage(client *firestore.CollectionRef, participantID string) *FirestoreDonationStorage {
+	return &FirestoreDonationStorage{
+		participantID:   participantID,
+		firestoreClient: client,
+	}
+}
+
+func (ds *FirestoreDonationStorage) Create(participantStorage service.ParticipantStorage, prizeStorage service.PrizeStorage, d *service.Donation) error {
+	exists, err := ds.Exists(d.ID)
+	if err != nil {
+		return fmt.Errorf("check donation exists: %w", err)
+	}
+
+	if exists {
+		return service.ErrDonationAlreadyExists
+	}
+
+	participant, err := participantStorage.Get(d.ParticipantID)
+	if err != nil {
+		return fmt.Errorf("check existence: %w", err)
+	}
+	d.ParticipantID = participant.ID
+
+	prize, err := prizeStorage.Get(d.PrizeID)
+	if err != nil {
+		return fmt.Errorf("check existence: %w", err)
+	}
+	d.PrizeID = prize.ID
+
+	d.TicketNumber = d.Amount / prize.TicketCost
+
+	if _, err := ds.firestoreClient.Doc(d.ID).Set(context.Background(), d); err != nil {
+		return fmt.Errorf("donation saving failed: %w", err)
+	}
+
+	return err
+}
+
+func (ds *FirestoreDonationStorage) Get(id string) (*service.Donation, error) {
+	doc, err := ds.firestoreClient.Doc(id).Get(context.Background())
+	if err != nil {
+		if isNotFound(err) {
+			return nil, service.ErrDonationNotFound
+		}
+		return nil, fmt.Errorf("get donation: %w", err)
+	}
+
+	var d service.Donation
+	if err := doc.DataTo(&d); err != nil {
+		return nil, fmt.Errorf("decode donation: %w", err)
+	}
+
+	return &d, nil
+}
+
+func (ds *FirestoreDonationStorage) GetAll() ([]service.Donation, error) {
+	docs, err := ds.firestoreClient.Documents(context.Background()).GetAll()
+	if err != nil {
+		return nil, fmt.Errorf("get all donations: %w", err)
+	}
+
+	donations := make([]service.Donation, 0, len(docs))
+	for _, doc := range docs {
+		var d service.Donation
+		if err = doc.DataTo(&d); err != nil {
+			return nil, fmt.Errorf("decoce donations: %w", err)
+		}
+
+		donations = append(donations, d)
+	}
+
+	return donations, nil
+}
+
+func (ds *FirestoreDonationStorage) Update(d *service.Donation) error {
+	exists, err := ds.Exists(d.ID)
+	if err != nil {
+		return fmt.Errorf("check donation exists: %w", err)
+	}
+
+	if !exists {
+		return service.ErrDonationNotFound
+	}
+
+	if _, err := ds.firestoreClient.Doc(d.ID).Set(context.Background(), d); err != nil {
+		return fmt.Errorf("update donation: %w", err)
+	}
+
+	return nil
+}
+
+func (ds *FirestoreDonationStorage) Delete(id string) error {
+	exists, err := ds.Exists(id)
+	if err != nil {
+		return fmt.Errorf("check donation exists: %w", err)
+	}
+
+	if !exists {
+		return service.ErrDonationNotFound
+	}
+
+	if _, err := ds.firestoreClient.Doc(id).Delete(context.Background()); err != nil {
+		return fmt.Errorf("delete donation: %w", err)
+	}
+
+	return nil
+}
+
+func (ds *FirestoreDonationStorage) Exists(id string) (bool, error) {
+	doc, err := ds.firestoreClient.Doc(id).Get(context.Background())
+	if isNotFound(err) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	return doc.Exists(), nil
+}
