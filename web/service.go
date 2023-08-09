b84b4a5d7992712fc5736645d16328cc8c6be757
delveper <yevhen@bilyk.dev>
2023-08-09 08:51:09 +0300

    refactor: roll bash serviceFunc abs

--- web/service.go
+++ web/service.go
@@ -22,12 +22,18 @@ type (
 // which fetches from request services implemented in the business logic layer.
 // It checks if underlying business layer service implements a collection of CRUD interfaces
 // and maps them to corresponding functions that handle requests.
-type Service[T, I, O any] func(*http.Request) (T, error)
+type Service[T, I, O any] ServiceFunc[T]
+
+// ServiceFunc is a function that fetches from request services implemented in the business logic layer.
+type ServiceFunc[T any] func(*http.Request) (T, error)
 
 func newService[T, I, O any](fn Service[T, I, O]) Service[T, I, O] { return fn }
 
+// Func is a method that casts service functions to ServiceFunc without I/O parameters.
+func (s Service[T, _, _]) Func() ServiceFunc[T] { return ServiceFunc[T](s) }
+
 func (s Service[T, I, _]) Create(rw http.ResponseWriter, req *http.Request) {
-	svc, err := extractService[T, Creator[I]](s, req)
+	svc, err := extractService[T, Creator[I]](s.Func(), req)
 	if err != nil {
 		respondErr(rw, err)
 		return
@@ -36,7 +42,7 @@ func (s Service[T, I, _]) Create(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (s Service[T, _, O]) Get(rw http.ResponseWriter, req *http.Request) {
-	svc, err := extractService[T, Getter[O]](s, req)
+	svc, err := extractService[T, Getter[O]](s.Func(), req)
 	if err != nil {
 		respondErr(rw, err)
 		return
@@ -45,7 +51,7 @@ func (s Service[T, _, O]) Get(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (s Service[T, I, _]) Edit(rw http.ResponseWriter, req *http.Request) {
-	svc, err := extractService[T, Editor[I]](s, req)
+	svc, err := extractService[T, Editor[I]](s.Func(), req)
 	if err != nil {
 		respondErr(rw, err)
 		return
@@ -54,7 +60,7 @@ func (s Service[T, I, _]) Edit(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (s Service[T, _, _]) Delete(rw http.ResponseWriter, req *http.Request) {
-	svc, err := extractService[T, Deleter](s, req)
+	svc, err := extractService[T, Deleter](s.Func(), req)
 	if err != nil {
 		respondErr(rw, err)
 		return
@@ -63,7 +69,7 @@ func (s Service[T, _, _]) Delete(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (s Service[T, _, O]) List(rw http.ResponseWriter, req *http.Request) {
-	svc, err := extractService[T, Lister[O]](s, req)
+	svc, err := extractService[T, Lister[O]](s.Func(), req)
 	if err != nil {
 		respondErr(rw, err)
 		return
@@ -72,8 +78,8 @@ func (s Service[T, _, O]) List(rw http.ResponseWriter, req *http.Request) {
 	newHandler[List[O]](svc.List).Handle(rw, req)
 }
 
-// extractService fetch a service T from service func using request data.
-func extractService[T, S any](fn func(*http.Request) (T, error), req *http.Request) (S, error) {
+// extractService fetch a service T from service func using request data
+func extractService[T, S any](fn ServiceFunc[T], req *http.Request) (S, error) {
 	var svc S
 
 	val, err := fn(req)
