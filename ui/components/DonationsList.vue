c247e48e95cc538046795a4890768880f8d4aa21
Artem Yadelskyi <mymmrac@gmail.com>
2023-08-16 00:42:45 +0300

    UI: Updated prize details

---
+++ ui/components/DonationsList.vue
@@ -0,0 +1,7 @@
+<template>
+    Donations (TBD)
+</template>
+
+<script setup lang="ts">
+
+</script>
--- ui/components/PrizeDetails.vue
+++ ui/components/PrizeDetails.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="rounded-lg bg-white p-3 shadow-md ring-1 ring-black ring-opacity-5">
-        <h2 class="text-xl">Деталі призу</h2>
-        <hr class="mt-2">
-        <div v-if="selectedPrize">
-            {{ selectedPrize.name }}
+        <div class="flex justify-between items-center gap-2">
+            <div class="text-xl">Деталі призу</div>
+            <div class="flex gap-2">
+                <TheButton :click="() => { isOpenUpdate = true} " :disabled="!selectedPrize">Змінити</TheButton>
+                <TheButton :click="() => { isOpenDelete = true }" danger :disabled="!selectedPrize">Видалити</TheButton>
+            </div>
         </div>
-        <div v-else>
+        <hr class="mt-2">
+        <div v-if="selectedPrize" class="flex flex-col gap-2 mt-2">
+            <div class="flex justify-between">
+                <div class="text-xl">{{ selectedPrize.name }}</div>
+                <div>Ціна купону: {{ selectedPrize.ticketCost }}</div>
+            </div>
+            <div v-if="selectedPrize.description" class="whitespace-pre">{{ selectedPrize.description }}</div>
+            <hr>
+            <div class="flex justify-between items-center gap-2">
+                <div>
+                    <div class="text-xl">Внески</div>
+                </div>
+                <div>
+                    <TheButton :click="() => { isOpenCreateDonation = true} ">Додати внесок</TheButton>
+                </div>
+            </div>
+            <DonationsList/>
+        </div>
+        <div v-else class="mt-2">
             Не вибрано
         </div>
     </div>
@@ -16,4 +36,8 @@ import { usePrizeStore } from "~/store/prize"
 
 const prizeStore = usePrizeStore()
 const { selectedPrize } = storeToRefs(prizeStore)
+
+const isOpenDelete = ref(false)
+const isOpenUpdate = ref(false)
+const isOpenCreateDonation = ref(false)
 </script>
