8947197a7e4249acfaea3d4034d36fcae26e2a9b
user <13639129+radikh@users.noreply.github.com>
2023-12-18 12:36:34 +0200

    Add router log

--- web/handler.go
+++ web/handler.go
@@ -25,12 +25,6 @@ type ListResponse[O any] struct {
 	Items []O `json:"items"`
 }
 
-func newCreate[I any](fn Create[I]) Create[I] { return fn }
-func newGet[O any](fn Get[O]) Get[O]          { return fn }
-func newEdit[I any](fn Edit[I]) Edit[I]       { return fn }
-func newDelete(fn Delete) Delete              { return fn }
-func newList[O any](fn List[O]) List[O]       { return fn }
-
 func (m Create[I]) Handle(rw http.ResponseWriter, req *http.Request) {
 	var in I
 	if err := decodeBody(req.Body, &in); err != nil {
