00da5d1534b5ee9ba66c59e3ae501b22e46cd5ff
user <13639129+radikh@users.noreply.github.com>
2024-03-03 19:19:59 +0200

    Init errors.

---
+++ structerror/errors_test.go
@@ -0,0 +1,193 @@
+package structerror
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	err := New("code", "test error")
+	assert.Error(t, err)
+}
+
+func TestCoder(t *testing.T) {
+	t.Run("Normal", func(t *testing.T) {
+		err := New("code", "test error")
+
+		var coder Coder = Coder(nil)
+		require.True(t, errors.As(err, &coder))
+		assert.Equal(t, "code", coder.Code())
+	})
+
+	t.Run("No code", func(t *testing.T) {
+		err := New("", "test error")
+
+		var coder Coder = Coder(nil)
+		require.True(t, errors.As(err, &coder))
+		assert.Equal(t, "", coder.Code())
+	})
+}
+
+func TestError(t *testing.T) {
+	t.Run("normal format", func(t *testing.T) {
+		err := New("code", "test error")
+		assert.Equal(t, "code: test error", err.Error())
+	})
+
+	t.Run("empty code", func(t *testing.T) {
+		err := New("", "test error")
+		assert.Equal(t, "test error", err.Error())
+	})
+
+	t.Run("empty error", func(t *testing.T) {
+		err := New("code", "")
+		assert.Equal(t, "code", err.Error())
+	})
+
+	t.Run("empty code and error", func(t *testing.T) {
+		err := New("", "")
+		assert.Equal(t, "", err.Error())
+	})
+
+	t.Run("fmt", func(t *testing.T) {
+		err := New("code", "test error: %s", "param")
+		assert.Equal(t, "code: test error: param", err.Error())
+	})
+}
+
+func TestWrap(t *testing.T) {
+	base := &net.AddrError{Err: "test error", Addr: "test addr"}
+	structured := New("code", "test error: %w", base)
+	wrapper := fmt.Errorf("wrapped error: %w", structured)
+
+	t.Run("as", func(t *testing.T) {
+		var wrappedErr *CodeError
+		require.True(t, errors.As(wrapper, &wrappedErr))
+		assert.Equal(t, "code", wrappedErr.Code())
+
+		t.Run("nested", func(t *testing.T) {
+			var addrErr *net.AddrError
+			require.True(t, errors.As(wrapper, &addrErr))
+			assert.Equal(t, base, addrErr)
+		})
+	})
+
+	t.Run("is", func(t *testing.T) {
+		t.Run("base", func(t *testing.T) {
+			assert.True(t, errors.Is(structured, base))
+		})
+
+		t.Run("wrapper", func(t *testing.T) {
+			assert.True(t, errors.Is(wrapper, structured))
+		})
+	})
+
+	t.Run("unwrap", func(t *testing.T) {
+		t.Run("base", func(t *testing.T) {
+			assert.Equal(t, base, errors.Unwrap(structured))
+		})
+
+		t.Run("wrapper", func(t *testing.T) {
+			assert.Equal(t, structured, errors.Unwrap(wrapper))
+		})
+	})
+
+	t.Run("wrap nil", func(t *testing.T) {
+		err := WithCode("code", nil)
+		assert.Equal(t, "code", err.Error())
+	})
+
+	t.Run("double wrap", func(t *testing.T) {
+		e1 := WithCode("code1", fmt.Errorf("test error"))
+		e2 := WithCode("code2", e1)
+		assert.Equal(t, "code2: code1: test error", e2.Error())
+
+		t.Run("as", func(t *testing.T) {
+			var wrappedErr *CodeError
+			require.True(t, errors.As(e2, &wrappedErr))
+			assert.Equal(t, "code2", wrappedErr.Code())
+		})
+
+		t.Run("is", func(t *testing.T) {
+			assert.False(t, errors.Is(e2, e1))
+		})
+
+		t.Run("join", func(t *testing.T) {
+			e3 := errors.Join(e2, e1)
+			assert.True(t, errors.Is(e3, e1))
+			assert.True(t, errors.Is(e3, e2))
+		})
+
+	})
+}
+
+func TestDeveloperUsecases(t *testing.T) {
+	t.Run("Wrap", func(t *testing.T) {
+		err := WithCode("code", fmt.Errorf("test error"))
+		assert.Equal(t, "code: test error", err.Error())
+	})
+
+	// t.Run("with value", func(t *testing.T) {
+	// 	err := New("code", "test error")
+	// 	withValue := WithValue(err, "key1", "value1")
+	// 	withValue = WithValue(err, "key2", "value2")
+
+	// 	assert.Equal(t, "code: test error: key1=value1, key2=value2", withValue.Error())
+	// 	assert.ErrorIs(t, withValue, err)
+	// })
+
+}
+
+type Coder interface {
+	Code() string
+}
+
+type CodeError struct {
+	code string
+	error
+}
+
+func New(code, format string, args ...any) error {
+	return &CodeError{code: code, error: fmt.Errorf(format, args...)}
+}
+
+func (e *CodeError) Error() string {
+	if e.code == "" {
+		return e.error.Error()
+	}
+
+	if e.error.Error() == "" {
+		return e.code
+	}
+
+	return e.code + ": " + e.error.Error()
+}
+
+func (e *CodeError) Unwrap() error {
+	return errors.Unwrap(e.error)
+}
+
+func (e *CodeError) Code() string {
+	return e.code
+}
+
+func WithCode(code string, err error) error {
+	if err == nil {
+		return &CodeError{code: code, error: errors.New("")}
+	}
+	return &CodeError{code: code, error: err}
+}
+
+type ValueError struct {
+	error
+	values map[string]string
+}
+
+func WithValue(err error, key, value string) error {
+	return err
+}
